0f11f11229fec36b1d1aae8dfbe237e0114f5177
chirag-unision <sharmacj123@gmail.com>
2024-09-02 00:52:21 +0530

    home page responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div className='w-full h-full text-white absolute top-0' style={{background: "url('https://wallpaperaccess.com/full/2481994.jpg')"}}>
         <div className='bg-black opacity-80 absolute h-screen w-screen'></div>
         <div className='flex justify-center items-center w-full h-full'>
-            <div className='w-2/3 flex flex-col items-center'>
+            <div className='md:w-2/3 w-full flex flex-col items-center'>
                 <motion.div 
                     initial={{
                         opacity:0,
@@ -29,7 +29,7 @@ const Home = () => {
                         duration: 1,
                         type: 'spring',
                     }}
-                    className='z-10 text-7xl font-semibold w-full p-4 text-center leading-tight'>Connect with your seniors, juniors and batchmates.</motion.div>
+                    className='z-10 text-4xl lg:text-7xl font-semibold w-full p-4 text-center leading-tight'>Connect with your seniors, juniors and batchmates.</motion.div>
                 <motion.div 
                     initial={{
                         opacity:0,
@@ -48,7 +48,7 @@ const Home = () => {
                         duration: 1,
                         type: 'spring',
                     }}
-                    className='z-10 text-2xl w-2/3 p-4 text-center font-thin'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi dolorum omnis quam perferendis vero dolor nemo iste itaque, totam neque enim facilis repudiandae ad voluptate minima nobis est officiis!</motion.div>
+                    className='z-10 text-md lg:text-2xl w-2/3 p-4 text-center font-thin'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi dolorum omnis quam perferendis vero dolor nemo iste itaque, totam neque enim facilis repudiandae ad voluptate minima nobis est officiis!</motion.div>
                 <div className='w-1/3 flex justify-around mt-4'>
                     {/* <PrimaryButton className={"w-full md:w-40 mx-2"}>Login</PrimaryButton>
                     <div className='p-2'></div>
