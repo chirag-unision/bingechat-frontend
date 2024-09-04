15939e0bc88be159de3a90ca0f7e00977829daf2
chirag-unision <sharmacj123@gmail.com>
2024-09-04 19:31:23 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext';
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import '../scrollbar.css';
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <AuthProvider>
     <App />
+    <Analytics />
   </AuthProvider>
   // </StrictMode>,
 )
